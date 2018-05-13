141c77073120aa3f04033c1cc39546247b4ebeac
אלדר <eldar.hacohen@mail.huji.ac.il>
2018-05-13 15:56:12 +0300

    . . . . .

--- ex4/src/AvlTree.java
+++ ex4/src/AvlTree.java
@@ -1,2 +1,30 @@
-public class AvlTree {
+public class AvlTree extends SimpleTree{
+    public AvlTree(){
+
+    }
+
+    @Override
+    public boolean add(int value) {
+
+        return false;
+    }
+
+//    private boolean add_helper(int value, Node root){
+//
+//    }
+
+    @Override
+    public boolean delete(int value) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(int value) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
